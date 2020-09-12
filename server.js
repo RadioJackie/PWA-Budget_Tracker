cb452e18a18133997a692c8109a8936148f63c85
RadioJackie <jacobpeters628@gmail.com>
2020-09-12 14:27:55 -0700

    connected to mongo db atlas

--- server.js
+++ server.js
@@ -19,10 +19,16 @@ mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
+// mongoose.connect('mongodb+srv://RadioJackie:FDZ273KJSrWHVrtl@cluster0.cpklf.mongodb.net/PWA-Budget-Tracker?retryWrites=true&w=majority'
+//   , {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// }).then(data => console.log("connected")).catch(data => console.log("not connected")) 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+// mongodb+srv://RadioJackie:FDZ273KJSrWHVrtl@cluster0.ew3kt.azure.mongodb.net/Project-0?retryWrites=true&w=majority
+//FDZ273KJSrWHVrtl
